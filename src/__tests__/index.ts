e53d93abd4ea243573ebe4294b9b33df769dc22c
weverton <wevertonmixalt@gmail.com>
2022-03-30 16:51:38 -0300

    updates

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,7 +1,11 @@
 import { getAllProductsName } from '../index';
 import daysSale from '../utils/data.json';
 
-test('add function exists', () => {
-  expect(getAllProductsName(daysSale))
-    .toHaveLength(14);
+test('check size of array', () => {
+  expect(getAllProductsName(daysSale).length).toBe(14);
+
 });
+
+test('check if all itens returned are string', () => {
+  expect(getAllProductsName(daysSale).every(item => typeof item === 'string')).toBe(true);
+})
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { Models } from "./types/Fruits";
 
-
 export const getAllProductsName = (days: Models.DaySale[]) => {
   return days.flatMap(day => day.products.map(product => product.name));
 }
