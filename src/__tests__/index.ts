c892ae70f09997b829c10446bf84c646b23ceb15
weverton <wevertonmixalt@gmail.com>
2022-03-30 16:46:16 -0300

    updates

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,12 +1,7 @@
 import { getAllProductsName } from '../index';
-
 import daysSale from '../utils/data.json';
 
-
-// test if add function exists
 test('add function exists', () => {
-  // test if the size of returned array is 14
   expect(getAllProductsName(daysSale))
     .toHaveLength(14);
-
 });
