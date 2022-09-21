05579b48baec1a7bbf0788bf9de2d6d82fcf96fd
weverton <wevertonmixalt@gmail.com>
2022-09-21 15:53:09 -0300

    tirando espacos

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
-import dotenv from "dotenv";
 import getAnswerAi from "./functions/getAnswerAi";
 import translateAi from "./functions/translateAi";
 
-dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app: Express = express();
@@ -17,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
   res.send("<h1>Hello from the TypeScript world!</h1>");
 });
+
 app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
 
 app.get("/question", async (req: Request, res: Response) => {
